ccae6e5392a217cf84d089a1c04a204c9655a4e4
swu-incomm <swu@incomm.com>
2019-11-14 15:49:04 -0500

    add str str

--- src/ImplementStrStr.java
+++ src/ImplementStrStr.java
@@ -19,7 +19,14 @@ For the purpose of this problem, we will return 0 when needle is an empty string
  */
 public class ImplementStrStr {
     public int strStr(String haystack, String needle) {
-        int ans = 0;
+        int ans = -1;
+        if(haystack == null || needle == null || needle.length() == 0) return 0;
+        for(int i = 0; i<=haystack.length()-needle.length(); i++) {
+            for(int j = 0; j<needle.length();j++) {
+                if(haystack.charAt(i + j) != needle.charAt(j)) break;
+                else if(j == needle.length() -1) return i;
+            }
+        }
         return ans;
     }
 }
