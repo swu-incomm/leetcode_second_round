5f2a349d432b64793e6fe489d89676ec376e1e89
swu-incomm <swu@incomm.com>
2019-11-22 17:07:18 -0500

    add symetric trees recursive

---
+++ src/SymmeticTree.java
@@ -0,0 +1,50 @@
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+ */
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class SymmeticTree {
+
+    //Recursive solution
+    public boolean isSymmetric(TreeNode root) {
+       if(root == null) return true;
+       return helper(root.left, root.right);
+    }
+    private boolean helper(TreeNode left, TreeNode right) {
+        //at lease one node is empty
+        if(left==null || right == null) return left == right;
+        else if(left.val != right.val) return false;
+        else return (helper(left.left, right.right) && (helper(left.right, right.left)));
+    }
+}
