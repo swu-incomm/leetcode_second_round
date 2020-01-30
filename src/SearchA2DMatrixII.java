d90a8f0ac6b73f92062aeb4dabf87b411b1098f2
swu-incomm <swu@incomm.com>
2020-01-30 17:21:06 -0500

    add search in 2 d array

---
+++ src/SearchA2DMatrixII.java
@@ -0,0 +1,33 @@
+/**
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ *
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * Example:
+ *
+ * Consider the following matrix:
+ *
+ * [
+ *   [1,   4,  7, 11, 15],
+ *   [2,   5,  8, 12, 19],
+ *   [3,   6,  9, 16, 22],
+ *   [10, 13, 14, 17, 24],
+ *   [18, 21, 23, 26, 30]
+ * ]
+ * Given target = 5, return true.
+ *
+ * Given target = 20, return false.
+ */
+public class SearchA2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) return false;
+        int m = 0;
+        int n = matrix[0].length-1;
+        while(m<matrix.length && n>=0) {
+            if(matrix[m][n] == target) return true;
+            else if(matrix[m][n] > target) {n--;}
+            else m++;
+        }
+        return false;
+    }
+}
