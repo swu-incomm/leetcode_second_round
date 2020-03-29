1a20792364a7b2f37f471c072c1ea52c91595825
Shubin Wu <shubinwu@Admins-MacBook-Pro-2.local>
2020-03-29 16:54:33 -0400

    add subtree of another tree

---
+++ src/SubtreeOfAnotherTree.java
@@ -0,0 +1,58 @@
+import apple.laf.JRSUIUtils;
+
+/**
+ * Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s.
+ * A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+ *
+ * Example 1:
+ * Given tree s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ * Given tree t:
+ *    4
+ *   / \
+ *  1   2
+ * Return true, because t has the same structure and node values with a subtree of s.
+ * Example 2:
+ * Given tree s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ *     /
+ *    0
+ * Given tree t:
+ *    4
+ *   / \
+ *  1   2
+ * Return false.
+ */
+public class SubtreeOfAnotherTree {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s == null || t == null) {
+            return false;
+        }
+        return treeToString(s, false).indexOf(treeToString(t, false))>= 0;
+    }
+    public String treeToString(TreeNode root, boolean isLeft) {
+        if(root == null) {
+            return isLeft ? "lnull" : "rnull";
+        }
+        return "#" + root.val + treeToString(root.left, true) + treeToString(root.right, false);
+    }
+
+    public static void main(String [] args) {
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        SubtreeOfAnotherTree subtreeOfAnotherTree = new SubtreeOfAnotherTree();
+        System.out.println(subtreeOfAnotherTree.treeToString(a, false));
+        System.out.println(subtreeOfAnotherTree.treeToString(b, false));
+
+    }
+}
