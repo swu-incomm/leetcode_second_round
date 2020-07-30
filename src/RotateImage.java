1136b3f9153a05676cfc895cc72b97676a466c03
swu-incomm <swu@incomm.com>
2020-07-29 23:24:40 -0400

    add rotate image

---
+++ src/RotateImage.java
@@ -0,0 +1,68 @@
+/**
+ * You are given an n x n 2D matrix representing an image.
+ *
+ * Rotate the image by 90 degrees (clockwise).
+ *
+ * Note:
+ *
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+ * DO NOT allocate another 2D matrix and do the rotation.
+ *
+ * Example 1:
+ *
+ * Given input matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ * Example 2:
+ *
+ * Given input matrix =
+ * [
+ *   [ 5, 1, 9,11],
+ *   [ 2, 4, 8,10],
+ *   [13, 3, 6, 7],
+ *   [15,14,12,16]
+ * ],
+ *
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ *   [15,13, 2, 5],
+ *   [14, 3, 4, 1],
+ *   [12, 6, 8, 9],
+ *   [16, 7,10,11]
+ * ]
+ */
+public class RotateImage {
+    public void rotate1(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+
+        int n = matrix.length;
+        int top = 0;
+        int left = 0;
+        int right = n-1;
+        int bottom = n-1;
+        while(n > 1) {
+            n-=2;
+            for(int i=0;i<n-1;i++) {
+                int temp = matrix[top][left + i];
+                matrix[top][left+i] = matrix[bottom-i][left];
+                matrix[bottom-i][left] = matrix[bottom][right-i];
+                matrix[bottom][right-i] = matrix[top+i][right];
+                matrix[top+i][right] = temp;
+            }
+            top++;
+            bottom--;
+            right--;
+            left++;
+        }
+    }
+}
