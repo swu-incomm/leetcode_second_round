3dfbf242c9094445b1e5fdeb3931e54b314755e1
swu-incomm <swu@incomm.com>
2020-08-02 22:31:48 -0400

    add coin change dp problem

---
+++ src/CoinChange.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+/**
+ * You are given coins of different denominations and a total amount of money amount.
+ * Write a function to compute the fewest number of coins that you need to make up that amount.
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
+ *
+ * Example 1:
+ *
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ * Example 2:
+ *
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ * Note:
+ * You may assume that you have an infinite number of each kind of coin.
+ */
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || coins.length == 0 || amount < 0) return -1;
+        int [] dp = new int [amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++) {
+            for(int j = 0; j<coins.length;j++) {
+                int temp = i - coins[j];
+                if(temp >= 0 && dp[temp] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[temp] + 1, dp[i]);
+                }
+            }
+        }
+      return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
--- src/PerfectSquares.java
+++ src/PerfectSquares.java
@@ -22,7 +22,7 @@ public class PerfectSquares {
         for(int i = 2; i<=n;i++) {
             int min = Integer.MAX_VALUE;
             for(int j=1; j*j<=i;j++) {
-                min = Math.min(min, dp[i-(j*j) + 1]);
+                min = Math.min(min, dp[i-(j*j)] + 1);
             }
             dp[i] = min;
         }
